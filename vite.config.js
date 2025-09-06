c9b5362d8cf6133391ba84ef8c7db53ab347455b
Thur aAung <thura.aung@comradesol.com>
2025-09-07 01:51:04 +0900

    force set path as temporary

--- vite.config.js
+++ vite.config.js
@@ -15,5 +15,5 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
-  base: process.env.NODE_ENV === 'production' ? '/jlpt-bunpou/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/jlpt-bunpou/' : '/jlpt-bunpou/',
 })
