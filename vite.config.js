26d910589e266592df383d0b691cc944b03672d4
Thur aAung <thura.aung@comradesol.com>
2025-09-07 01:53:49 +0900

    dns server setting seems works so temporary del

--- vite.config.js
+++ vite.config.js
@@ -15,5 +15,4 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
-  base: process.env.NODE_ENV === 'production' ? '/jlpt-bunpou/' : '/jlpt-bunpou/',
 })
